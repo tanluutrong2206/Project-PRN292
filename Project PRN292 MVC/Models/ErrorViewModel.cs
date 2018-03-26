23e81c702a38939530c26450bb94d1a17567449d
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2018-03-26 16:57:54 +0700

    Add project files.

---
+++ Project PRN292 MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_PRN292_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
